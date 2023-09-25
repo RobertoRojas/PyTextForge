51bbb491eae341661988a89d8b0d2b45b83492c6
Roberto Rojas <developer@robertorojas.com.mx>
2023-09-24 22:52:41 -0600

    Fixing issue with linux python and adding descriptions into the arguments

--- pytextforge/src/__main__.py
+++ pytextforge/src/__main__.py
@@ -26,7 +26,7 @@ def validate_output(outputpath: str, force: bool) -> str:
 def validate_file(value: str) -> str:
     if not os.path.isfile(path=value):
         raise argparse.ArgumentTypeError(f"The template path[{value}] doesn't exist or is not a file")
-    return os.path.realpath(path=value)
+    return os.path.realpath(value)
 
 def __validate(value: str, load: any) -> dict:
     id, data, err = load(string=value)
@@ -43,19 +43,19 @@ def validate_json(value: str) -> dict:
 def parse_arguments(args: list) -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='TODO')
     parser.add_argument('--version', action='version', version=VERSION, \
-                        help='TODO')
+                        help='Shows the version of the module')
     parser.add_argument('--template', dest='template', type=validate_file, \
-                        required=True, help='TODO')
+                        required=True, help='Set the template file path')
     parser.add_argument('--output', dest='output', type=str, \
-                        required=True, help='TODO')
+                        required=True, help='Set the output file path, if the file already exist, use --force to overwrite it')
     parser.add_argument('-d', '--data', action='append', dest='data', type=validate_data, \
-                        help='TODO')
+                        help='Set data in plain text')
     parser.add_argument('-j', '--json', action='append', dest='data', type=validate_json, \
-                        help='TODO')
+                        help='Set data in JSON format')
     parser.add_argument('-f', '--force', action='store_true', dest='force', \
-                        help='TODO')
+                        help='Delete the output file if exist')
     parser.add_argument('-v', '--verbose', action='count', default=0, dest='verbose', \
-                        help='TODO')
+                        help='Show verbose messages, can use more than one to increase the verbosity')
     return parser.parse_args(args=args)
 
 def exit_script(errorcode: int) -> None:
